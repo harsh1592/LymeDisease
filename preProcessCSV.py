1e4e86a616c8b4ebf94557dd7f8eb4a407eee6dd
harsh1592 <harsh1592@gmail.com>
2017-03-03 19:37:54 -0500

    updated preprocess script

--- preProcessCSV.py
+++ preProcessCSV.py
@@ -3,28 +3,29 @@ import sys
 from bs4 import BeautifulSoup
 
 filename = sys.argv[1]
+data =[]
 with open(filename) as csvfile:
 	readCSV = csv.reader(csvfile, delimiter=',')
 	for row in readCSV:
-		ThreadRef = row[0]
-		ReplyNum = row[1]
-		PosterID = row[2]
-		Content = row[3]
-		notes = row[4]
-		PostedOn = row[5]
-		PosterName = row[6]
-		Gathered = row[7]
-		
-		dateStr = str(PostedOn)
-		print(dateStr[:4]+"/"+dateStr[4:6]+"/"+dateStr[6:8]+" "+dateStr[8:10]+":"+dateStr[10:12])
-		
+		line =[]
+		Content = row[3]		
+		dateStr = str(row[5])
+		dateStamp = dateStr[:4]+"/"+dateStr[4:6]+"/"+dateStr[6:8]+" "+dateStr[8:10]+":"+dateStr[10:12]
 		Content = Content.replace(","," ")
-		#Content = Content.replace("<br />"," ")
+		Content = Content.replace(";"," ")
 		Content = Content.replace("\n"," ")
 		Content = Content.replace("\t"," ")
-		soup = BeautifulSoup(Content)
-		#Content = Content.replace("<(?:[^>=]|='[^']*'|=\"[^\"]*\"|=[^'\"][^\s>]*)*>","")		
-		print( soup.get_text())
+		soup = BeautifulSoup(Content,"lxml")		
+		text = soup.get_text()
+		
+		line.append(row[0])
+		line.append(row[1])
+		line.append(row[2])
+		line.append(row[6])
+		line.append(dateStamp)
+		line.append(text)
+		data.append(line)
 
-		print("\n")
-    
+with open(sys.argv[2], 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
