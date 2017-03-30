883c519c27ddd33c283d6d7aa81d8477f3e27ab1
Harsh Patil <Harsh Patil>
2017-03-30 14:45:50 -0400

    Updated time series analysis

--- time_series_analyze.py
+++ time_series_analyze.py
@@ -7,79 +7,28 @@ import pickle
 import sys
 import os
 import operator
-
+import pdb
+import nltk
 import pandas as pd
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import liwc.categories
+import concurrent
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing.pool import ThreadPool as Pool
+
 
 labels = ["first_person","second_person","third_person","posemo","negemo","cognitive","sensory","time","past","present","future","work","leisure","swear","social","family","friend","humans","anx","anger","sad","body","health","sexual","space","time","achieve","home","money","relig","Affect","cause","Quant","Numb","inhib","ingest","motion","nonfl","filler","number_classified_words","number_words"]
- 
-#sys.argv[1] = '10'  #A HACK FOR JUPYTER
-
-#def main():
     
 ListOfDicts=[]
 
-for filename in os.listdir("."):
-    D={}
-    if not filename.startswith("%s_di" % sys.argv[1]):
-        continue
-    g = nx.read_gexf(filename) #HACK for top dir
-    g.remove_edges_from(g.selfloop_edges())
-    D['numNodes/10']=len(g.nodes())/10
 
-    # find elements with highest core number
-    sorted_by_core_number = sorted(nx.core_number(g).items(), key=operator.itemgetter(1))
-    max_core_number = sorted_by_core_number[-1][1]
-
-    # report on strongly connected components
-    size_of_sccs = [len(x) for x in nx.strongly_connected_components(g)]
-    #print (filename)
-    D['filename']=filename[10:-5]
-    
-    #print ("max_core_number: %d" % max_core_number)
-    D['max_core_number']=max_core_number    
-
-    #print ("nontrivial sccs:")
-    #print ([x for x in size_of_sccs if x > 1])
-    D['nontrivial sccs'] = ([x for x in size_of_sccs if x > 1])
-    D['len NT sccs']  = len(D['nontrivial sccs'])
-
-
-    g.remove_nodes_from([x for x,y in nx.core_number(g).items() if y == max_core_number])
-
-    sorted_by_core_number = sorted(nx.core_number(g).items(), key=operator.itemgetter(1))
-    max_core_number = sorted_by_core_number[-1][1]
-
-    # report on strongly connected components
-    size_of_sccs = [len(x) for x in nx.strongly_connected_components(g)]
-
-    #print ("max_core_number: %d" % max_core_number)
-    D['NT max_core_number'] = max_core_number
-    
-    #print ("nontrivial sccs:")
-    #print ([x for x in size_of_sccs if x > 1])
-    D['NT nontrivial sccs'] = ([x for x in size_of_sccs if x > 1])
-    D['len NT nontrivial sccs'] = len(D['NT nontrivial sccs'])
-
-    #print ("")
-    ListOfDicts.append(D)
-    
-    
 def order(frame,var):
     varlist =[w for w in frame.columns if w not in var]
     frame = frame[var+varlist]
     return frame 
-
-df=pd.DataFrame(ListOfDicts)
-df=order(df,['filename', 'max_core_number',  'nontrivial sccs','NT nontrivial sccs','NT max_core_number' ] )
-#matplotlib inline
-dp = df.plot()
-dp.legend(loc='upper left', fontsize=7)
-
 
 def get_graph_measures(g):
         retdic = dict()
@@ -111,19 +60,19 @@ def getLIWCDictForText (text):
     global labels
 
     d = dict()
-
+    print "getting",len(text)
     vals = liwc.categories.classify(text)
-
     for i in range(0,len(vals)):
-        d[labels[i]] = 100 * float(vals[i])/float(vals[40])
-    d[labels[40]] = float(vals[40])
-
+        d[labels[i]] = 100 * vals[i]/float(vals[40])
+    # d[labels[40]] = float(vals[40])
+    print "got"
     return d
 
 def getLIWC (g):
+    print 'getting'
     postsText = " ".join([g.node[x]['text'] for x in g])
     commentsText = " ".join([g.edge[x][y]['text'] for x,y in g.edges()])
-
+    print postsText
     return (getLIWCDictForText(postsText), getLIWCDictForText(commentsText))
 
 def drawMe (listOfDicts, fileName,window):
@@ -136,18 +85,15 @@ def drawMe (listOfDicts, fileName,window):
     plt.clf()
     file('%s-graph-%s.html' % (window,fileName),'w').write(df.to_html())
     file('%s-graph-%s.csv' % (window,fileName),'w').write(df.to_csv())
-    
-listOfDicts=[]
-listOfLIWCDictsPosts = []
-listOfLIWCDictsComments = []
 
-for filename in os.listdir("."):
-    #print filename
+def tryMultiThread(file):
+    print "inside multi"
     D={}
-    if not filename.startswith("%s_di" % sys.argv[1]):
-        continue
-    print "Processing %s" % filename
-    g = nx.read_gexf(filename) #HACK for top dir
+    if not file.startswith("%s_di" % sys.argv[1]):
+        return
+    print "Processing %s" % file
+    g = nx.read_gexf(file) #HACK for top dir
+    print 'read gefx'
     (posts, comments) = getLIWC(g)
     print "Got LIWC"
     listOfLIWCDictsPosts.append (posts)
@@ -156,16 +102,77 @@ for filename in os.listdir("."):
     g.remove_edges_from(g.selfloop_edges())
     listOfDicts.append( get_graph_measures(g) )
     print "Got graph measures"
-    
 
-    
-drawMe(listOfDicts, "graph", sys.argv[1])
-drawMe(listOfLIWCDictsPosts, "LIWC-posts", sys.argv[1])
-drawMe(listOfLIWCDictsComments, "LIWC-comments", sys.argv[1])
+def main():
+    count=0
+    for filename in os.listdir("."):
+        print "reading", count
+        count += 1
+        D={}
+        if not filename.startswith("%s_di" % sys.argv[1]):
+            continue
+        g = nx.read_gexf(filename) #HACK for top dir
+        g.remove_edges_from(g.selfloop_edges())
+        D['numNodes/10']=len(g.nodes())/10
+
+        # find elements with highest core number
+        sorted_by_core_number = sorted(nx.core_number(g).items(), key=operator.itemgetter(1))
+        max_core_number = sorted_by_core_number[-1][1]
+
+        # report on strongly connected components
+        size_of_sccs = [len(x) for x in nx.strongly_connected_components(g)]
+        #print (filename)
+        D['filename']=filename[10:-5]
+        
+        #print ("max_core_number: %d" % max_core_number)
+        D['max_core_number']=max_core_number    
+
+        #print ("nontrivial sccs:")
+        #print ([x for x in size_of_sccs if x > 1])
+        D['nontrivial sccs'] = ([x for x in size_of_sccs if x > 1])
+        D['len NT sccs']  = len(D['nontrivial sccs'])
 
 
+        g.remove_nodes_from([x for x,y in nx.core_number(g).items() if y == max_core_number])
 
+        sorted_by_core_number = sorted(nx.core_number(g).items(), key=operator.itemgetter(1))
+        max_core_number = sorted_by_core_number[-1][1]
 
-#if __name__ == '__main__': 
-#    main()
-#df
+        # report on strongly connected components
+        size_of_sccs = [len(x) for x in nx.strongly_connected_components(g)]
+
+        #print ("max_core_number: %d" % max_core_number)
+        D['NT max_core_number'] = max_core_number
+        
+        #print ("nontrivial sccs:")
+        #print ([x for x in size_of_sccs if x > 1])
+        D['NT nontrivial sccs'] = ([x for x in size_of_sccs if x > 1])
+        D['len NT nontrivial sccs'] = len(D['NT nontrivial sccs'])
+
+        #print ("")
+        ListOfDicts.append(D)
+        print "Read"
+        #pdb.set_trace()
+    df=pd.DataFrame(ListOfDicts)
+    df=order(df,['filename', 'max_core_number',  'nontrivial sccs','NT nontrivial sccs','NT max_core_number' ] )
+    #matplotlib inline
+    dp = df.plot()
+    dp.legend(loc='upper left', fontsize=7)
+
+    listOfDicts = []
+    listOfLIWCDictsPosts = []
+    listOfLIWCDictsComments = []
+
+    # multithreading to speed the processing time 
+    print "thread start"
+    executor = concurrent.futures.ProcessPoolExecutor(1)
+    future = [executor.submit(tryMultiThread, filename) for filename in os.listdir(".")]
+    concurrent.futures.wait(future)
+    print "thread end"
+
+    drawMe(listOfDicts, "graph", sys.argv[1])
+    drawMe(listOfLIWCDictsPosts, "LIWC-posts", sys.argv[1])
+    drawMe(listOfLIWCDictsComments, "LIWC-comments", sys.argv[1])
+
+if __name__ == '__main__': 
+    main()
